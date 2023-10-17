13578d4fee96fff9274329870aa1ca178968fbeb
KB <kb@prashants-MacBook-Pro-2.local>
2023-10-17 13:27:47 +0530

    cardCarouselTopAlbums-qtify-takehome

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "./Section.module.css";
 import { CircularProgress } from "@mui/material";
 import Card from "../Card/Card";
+import Carousel from "../Carousel/Carousel";
 
 const Section = ({ title, data, type }) => {
   const [carousalToggle, setCarousalToggle] = useState(true);
@@ -27,7 +28,7 @@ const Section = ({ title, data, type }) => {
               })}
             </div>
           ) : (
-            <></>
+            <Carousel data={data} renderComponent={(data)=><Card data={data} type={type} />}/>
           )}
         </div>
       )}

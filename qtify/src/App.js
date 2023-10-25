9cb6ff8b8ae8c6011cf6c71e3d3a5f502501907c
KB <kb@prashants-MacBook-Pro-2.local>
2023-10-25 13:12:13 +0530

    tabsFilter-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,46 +1,124 @@
-
-import './App.css';
-import HeroImage from './components/HeroImage/HeroImage';
-import Navbar from './components/Navbar/Navbar';
-import Section from './components/Section/Section';
-import { useState,useEffect } from 'react';
-import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from './api/api';
+import "./App.css";
+import HeroImage from "./components/HeroImage/HeroImage";
+import Navbar from "./components/Navbar/Navbar";
+import Section from "./components/Section/Section";
+import { useState, useEffect } from "react";
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./api/api";
 
 function App() {
-  const [topAlbumsData,setTopAlbumsData] = useState([]);
-  const getTopsAlbumData =async() =>{
-    const data = await fetchTopAlbums();
-    setTopAlbumsData(data);
-  }
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+  const [newAlbumsData, setNewAlbumsData] = useState([]);
+  const [songsData, setSongsData] = useState([]);
+  const [toggle, setToggle] = useState(false);
+  const [filteredDataValues, setFilteredDataValues] = useState([]);
+  const [value, setValue] = useState(0);
 
-  const [newAlbumsData,setNewAlbumsData] = useState([]);
-  const getNewAlbumData =async() =>{
-    const data = await fetchNewAlbums();
-    setNewAlbumsData(data);
-  }
+  const getTopsAlbumData = async () => {
+    try {
+      const data = await fetchTopAlbums();
+      setTopAlbumsData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  const [songsData,setSongsData] = useState([]);
-  const getSongsData =async() =>{
-    const data = await fetchSongs();
-    setSongsData(data);
-  }
+  const getNewAlbumData = async () => {
+    try {
+      const data = await fetchNewAlbums();
+      setNewAlbumsData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  useEffect(()=>{
+  const getAllSongsData = async () => {
+    try {
+      const data = await fetchSongs();
+      setSongsData(data);
+      setFilteredDataValues(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const filteredData = (res) => {
+    setFilteredDataValues(res);
+  };
+
+  const generateSongsData = (value) => {
+    let key;
+
+    switch (value) {
+      case 1:
+        key = "rock";
+        break;
+      case 2:
+        key = "pop";
+        break;
+      case 3:
+        key = "jazz";
+        break;
+      case 4:
+        key = "blues";
+        break;
+      default:
+        filteredData(songsData);
+        return;
+    }
+    const res = songsData.filter((item) => item.genre.key === key);
+
+    filteredData(res);
+  };
+
+  useEffect(() => {
     getTopsAlbumData();
     getNewAlbumData();
-    getSongsData();
-  },[])
+    getAllSongsData();
+  }, []);
+
+  useEffect(() => {
+    generateSongsData(value);
+  }, [value]);
+
   return (
     <div>
-     <Navbar />
-     <HeroImage />
-     <div>
-      <Section data ={topAlbumsData} type="album" title="Top Albums"/>
-      <Section data ={newAlbumsData} type="album" title="New Albums"/>
-      <Section data ={songsData} type="song" title="Songs"/>
-     </div>
-    
-    
+      <Navbar />
+      <HeroImage />
+      <div>
+        <Section
+          data={topAlbumsData}
+          type="album"
+          title="Top Albums"
+          filteredDataValues={topAlbumsData}
+          handleToggle={handleToggle}
+          toggle={toggle}
+        />
+        <Section
+          data={newAlbumsData}
+          type="album"
+          title="New Albums"
+          filteredDataValues={newAlbumsData}
+          handleToggle={handleToggle}
+          toggle={toggle}
+        />
+        <Section
+          data={songsData}
+          type="song"
+          title="Songs"
+          filteredData={filteredData}
+          filteredDataValues={filteredDataValues}
+          value={value}
+          handleToggle={handleToggle}
+          handleChange={handleChange}
+          toggle={toggle}
+        />
+      </div>
     </div>
   );
 }
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,34 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Section.module.css";
 import { CircularProgress } from "@mui/material";
 import Card from "../Card/Card";
 import Carousel from "../Carousel/Carousel";
+import BasicTabs from "../Tabs/BasicTabs";
 
-const Section = ({ title, data, type }) => {
-  const [carousalToggle, setCarousalToggle] = useState(true);
-  const handleToggle = () => {
-    setCarousalToggle(!carousalToggle);
-  };
+const Section = ({ title, data, type,filteredData=null,filteredDataValues=[],toggle=false,handleToggle=null,value=0,handleChange=null }) => {
+  //const [carousalToggle, setCarousalToggle] = useState(true);
+//   const handleToggle = () => {
+//     setCarousalToggle(!carousalToggle);
+//   };
   return (
     <div  className={styles.border}>
       <div className={styles.header}>
         <h3>{title}</h3>
         <h4 className={styles.toggleText} onClick={handleToggle}>
-          {!carousalToggle ? "Collapse all" : "Show All"}
+          {!toggle?"Collapse all" : "Show All"}
         </h4>
       </div>
+      {type==="song"?<BasicTabs value={value} handleChange={handleChange}/>:null}
       {data.length === 0 ? (
         <CircularProgress />
       ) : (
         <div className={styles.cardWrapper} >
-          {!carousalToggle ? (
+          {!toggle ? (
             <div className={styles.wrapper}>
-              {data.map((item) => {
+              {filteredDataValues.map((item) => {
                 return <Card data={item} type={type} />;
               })}
             </div>
           ) : (
-            <Carousel data={data} renderComponent={(data)=><Card data={data} type={type} />}/>
+            <Carousel data={filteredDataValues} renderComponent={(filteredDataValues)=><Card data={filteredDataValues} type={type} />}/>
           )}
         </div>
       )}
